68946adb51d1f5aa339a043fefdec246ee9eab8e
anisanursekararum <anisanursekararum@gmail.com>
2023-06-20 21:43:21 +0700

    setup driver using marven dependency

---
+++ src/test/java/ui/DemoAutomation.java
@@ -0,0 +1,21 @@
+package ui;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoAutomation {
+	
+	public static void main(String[] args) {
+		
+		//System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.ebay.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("mobile");
+		driver.findElement(By.xpath("//*[@id=\"gh-btn\"]")).click();
+		driver.close();
+	}
+}
